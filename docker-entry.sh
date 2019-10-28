e20c546c6e34f61afcdadecd0604bf9b92d13f4d
Fabian Große <saphareas@googlemail.com>
2019-10-28 22:27:03 +0100

    Fix typo

--- docker-entry.sh
+++ docker-entry.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Sign listed
-if [ "$INPUT_SIGN_LISTED" = true ]; then
+if [ "$SIGN_LISTED" = true ]; then
     echo "Signing listed extension..."
-    web-ext --channel=listed
+    web-ext sign --channel=listed
     echo "Done."
 
 # Sign unlisted
 else
     echo "Signing unlisted extension..."
-    web-ext --channel=unlisted
+    web-ext sign --channel=unlisted
     echo "Done. The file is ready to be used."
 fi
