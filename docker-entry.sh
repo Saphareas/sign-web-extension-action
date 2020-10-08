63d0e31209dd383bdc8aca1b754fc54a397ab768
issamouu <issamouu69@gmail.com>
2020-10-08 14:18:06 +0200

    feat: extend script to support io dir
    - Add -s flag to web-ext.
    
    - In case of an unlisted extension move the xpi file to the desired
    target directory.

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,12 +4,13 @@ set -e
 # Sign listed
 if [ "$SIGN_LISTED" = true ]; then
     echo "Signing listed extension..."
-    web-ext sign --channel=listed
+    web-ext sign -s "$WEB_EXT_SOURCE_DIR" --channel=listed
     echo "Done."
 
 # Sign unlisted
 else
     echo "Signing unlisted extension..."
-    web-ext sign --channel=unlisted
+    web-ext sign -s "$WEB_EXT_SOURCE_DIR" --channel=unlisted
+    mv *.xpi "$WEB_EXT_TARGET_DIR"
     echo "Done. The file is ready to be used."
 fi
