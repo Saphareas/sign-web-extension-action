a3dc24165556c58620de8d3c83cd8ca465c014bd
issamouu <issamouu69@gmail.com>
2020-10-08 15:55:55 +0200

    refactor: add move_to_target function

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 set -e
 
+# Move the newly created xpi files to taget dir
+function move_to_target() {
+    if [ "$WEB_EXT_TARGET_DIR" != "." ]; then
+        mv $(ls -t web-ext-artifacts/*.xpi | head -1) $WEB_EXT_TARGET_DIR
+    fi
+}
+
 # Sign listed
 if [ "$SIGN_LISTED" = true ]; then
     echo "Signing listed extension..."
     web-ext sign -s "$WEB_EXT_SOURCE_DIR" --channel=listed
+    move_to_target
     echo "Done."
 
 # Sign unlisted
 else
     echo "Signing unlisted extension..."
     web-ext sign -s "$WEB_EXT_SOURCE_DIR" --channel=unlisted
-    # if target directory is set move xpi file
-    if [ "$WEB_EXT_TARGET_DIR" != "." ]; then
-        # Move newly created file to desired target dir
-        mv $(ls -t web-ext-artifacts/*.xpi | head -1) $WEB_EXT_TARGET_DIR
-    fi
+    move_to_target
     echo "Done. The file is ready to be used."
 fi
